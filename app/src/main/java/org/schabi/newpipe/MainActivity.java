7119501ad9eab32222f968ca7aecc5eea4aef0e2
luka <trungld2@vng.com.vn>
2025-08-08 16:32:41 +0700

    Sync changes with NewPipe 0.28.0

--- app/src/main/java/org/schabi/newpipe/MainActivity.java
+++ app/src/main/java/org/schabi/newpipe/MainActivity.java
@@ -89,7 +89,6 @@ import org.schabi.newpipe.player.event.OnKeyDownListener;
 import org.schabi.newpipe.player.helper.PlayerHolder;
 import org.schabi.newpipe.player.playqueue.PlayQueue;
 import org.schabi.newpipe.settings.UpdateSettingsFragment;
-import org.schabi.newpipe.settings.migration.MigrationManager;
 import org.schabi.newpipe.util.Constants;
 import org.schabi.newpipe.util.DeviceUtils;
 import org.schabi.newpipe.util.ITubeUtils;
@@ -279,7 +278,6 @@ public class MainActivity extends AppCompatActivity {
                 UpdateSettingsFragment.askForConsentToUpdateChecks(this);
             }
         }
-        MigrationManager.showUserInfoIfPresent(this);
         iTubeAppViewModel.checkNewVersion();
     }
 
